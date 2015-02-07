bcfdb999a4fbb532cf41d8e760fff01aa3dca10b
Josh Lewis <josh.w.lewis@gmail.com>
2015-02-07 09:59:20 -0600

    Refactor tests and get passing.

--- tests/unit/routes/projects/project-test.js
+++ tests/unit/routes/projects/project-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('route:project', 'ProjectRoute', {
+moduleFor('route:projects/project', 'ProjectRoute', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
